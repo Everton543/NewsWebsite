a0d143f9050b30e9d69627a3a66f6625b393f887
Everton Alves <64697974+Everton543@users.noreply.github.com>
2024-03-30 20:22:14 -0300

    Create the base HTML

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.set('view engine', 'ejs');
 
 const homeRoutes = require('./routes/homeRoutes');
 const newsRoutes = require('./routes/newsRoutes');
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,34 @@
+const fs = require('fs');
 const path = require('path');
 
-exports.index = (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/home.html'));
+function readFileAsync(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) reject(err);
+            else resolve(data);
+        });
+    });
+}
+
+exports.index = async (req, res) => {
+    try {
+        const layoutPath = path.join(__dirname, '../public/layout.html');
+        const contentPath = path.join(__dirname, '../views/home.html');
+
+        // Read layout and content files asynchronously
+        const layout = await readFileAsync(layoutPath);
+        const content = await readFileAsync(contentPath);
+
+        // Replace placeholders in layout with content and other dynamic values
+        let htmlContent = layout
+            .replace('<$title>', 'Home')
+            .replace('<$css>', '')//'<link rel="stylesheet" href="/css/style.css">')
+            .replace('<$js>', '')//'<script src="/js/script.js"></script>')
+            .replace('<$content>', content);
+
+        res.send(htmlContent);
+    } catch (err) {
+        console.error("Error loading page:", err);
+        res.status(500).send('Error loading page');
+    }
 };
